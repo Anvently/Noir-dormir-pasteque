fe5d6883ad81556f813f6bc24a0fa20c8d7744c3
Anvently <pirard.nicolas@hotmail.fr>
2022-09-13 20:01:02 +0200

    fin de soiree

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const app = express();
 
+const deck = ["Les grands donateurs de la banque de sperme","Le botox","21cm de bonheur","un autocollant 'enfant à bord'","le botox","petits efforts, gros résultats"];
+
+const players = [
+	{name: "herroZ", password: "bbb", id: 2, cards: ["Les grands donateurs de la banque de sperme","Le botox","21cm de bonheur","un autocollant 'enfant à bord'","le botox","petits efforts, gros résultats"],cards_played: ["l'aspirine","pépé dans mémé"]},
+	{name: "herroB", password: "ccc", id: 3, cards: ["Les grands donateurs de la banque de sperme","Le botox","21cm de bonheur","un autocollant 'enfant à bord'","le botox","petits efforts, gros résultats"],cards_played: ["l'aspirine","pépé dans mémé"]},
+	{name: "Magie", password: "ddd", id: 4, cards: ["Les grands donateurs de la banque de sperme","Le botox","21cm de bonheur","un autocollant 'enfant à bord'","le botox","petits efforts, gros résultats"],cards_played: ["l'aspirine","pépé dans mémé"]},
+	{name: "TOM", password: "eee", id: 5, cards: ["Les grands donateurs de la banque de sperme","Le botox","21cm de bonheur","un autocollant 'enfant à bord'","le botox","petits efforts, gros résultats"],cards_played: ["l'aspirine","pépé dans mémé"]},	
+	{name: "TEX", password: "fff", id: 6, cards: ["Les grands donateurs de la banque de sperme","Le botox","21cm de bonheur","un autocollant 'enfant à bord'","le botox","petits efforts, gros résultats"],cards_played: ["l'aspirine","pépé dans mémé"]}	
+];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+
+}
+function generateCards(cards=[]) {
+	while (!cards || cards.length < 7) {
+		cards.push(deck[getRandomInt(deck.length)]);
+	}
+	return cards;
+	
+}
 
 app.use(express.json());
 
@@ -16,25 +37,17 @@ app.get('/test', (req, res, next) => {
   res.status(200).end("test");
 });
 
-app.get('/api/stuff', (req, res, next) => {
-  const stuff = [
-    {
-      _id: 'oeihfzeoi',
-      title: 'Mon premier objet',
-      description: 'Les infos de mon premier objet',
-      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-      price: 4900,
-      userId: 'qsomihvqios',
-    },
-    {
-      _id: 'oeihfzeomoihi',
-      title: 'Mon deuxième objet',
-      description: 'Les infos de mon deuxième objet',
-      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-      price: 2900,
-      userId: 'qsomihvqios',
-    },
-  ];
+app.post('/register_player', (req, res, next) => {
+  var id=players.length+1;
+  var crypto = require("crypto");
+  var pswd = crypto.randomBytes(20).toString('hex');
+  player={name: req.name, password: pswd, id: id, cards: generateCards(), cards_played: null};
+  players.push(player);
+  console.log(player);
+  res.status(201).json(player);
+});
+
+app.get('/cards', (req, res, next) => {
   res.status(200).json(stuff);
 });
 
