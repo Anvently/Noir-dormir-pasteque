637a1dd5c2a4a2a8c55f2209ebfd5ff982d5063b
herroP <pirard.nicolas@hotmail.fr>
2022-09-19 21:26:17 +0200

    gros ajout / reste à gérer l'initQuestion pour vider d'abord le boargros ajout / reste à gérer l'initQuestion pour vider d'abord le board gros ajout / reste à gérer l'initQuestion pour vider d'abord le boargros ajout / reste à gérer l'initQuestion pour vider d'abord le boardPlease enter the commit message for your changes. Lines starting Changes to be committed: odified:   backend/app.js

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const app = express();
 const bp = require('body-parser');
+const questions = require('./questions.json');
 
 const deck = ["Les grands donateurs de la banque de sperme","Le botox","21cm de bonheur","un autocollant 'enfant à bord'","le botox","petits efforts, gros résultats"];
 
 const players = [
-	{name: "herroZ", password: "bbb", id: 1, cards: ["Les grands donateurs de la banque de sperme","Le botox","21cm de bonheur","un autocollant 'enfant à bord'","le botox","petits efforts, gros résultats"],cards_played: ["l'aspirine","pépé dans mémé"]},
-	{name: "herroB", password: "ccc", id: 2, cards: ["Les grands donateurs de la banque de sperme","Le botox","21cm de bonheur","un autocollant 'enfant à bord'","le botox","petits efforts, gros résultats"],cards_played: ["l'aspirine","pépé dans mémé"]},
-	{name: "Magie", password: "ddd", id: 3, cards: ["Les grands donateurs de la banque de sperme","Le botox","21cm de bonheur","un autocollant 'enfant à bord'","le botox","petits efforts, gros résultats"],cards_played: ["l'aspirine","pépé dans mémé"]},
-	{name: "TOM", password: "eee", id: 4, cards: ["Les grands donateurs de la banque de sperme","Le botox","21cm de bonheur","un autocollant 'enfant à bord'","le botox","petits efforts, gros résultats"],cards_played: ["l'aspirine","pépé dans mémé"]},	
-	{name: "TEX", password: "fff", id: 5, cards: ["Les grands donateurs de la banque de sperme","Le botox","21cm de bonheur","un autocollant 'enfant à bord'","le botox","petits efforts, gros résultats"],cards_played: ["l'aspirine","pépé dans mémé"]}	
+	{name: "herroZ", updates: [], password: "bbb", id: 1, cards: ["Les grands donateurs de la banque de sperme","Le botox","21cm de bonheur","un autocollant 'enfant à bord'","le botox","petits efforts, gros résultats"],cards_played: ["l'aspirine","pépé dans mémé"]},
+	
 ];
+
+const player_list = [
+	{name: "herroZ", id: 1, nbr_votes: 0, score: 0, cards_played: []},
+	];
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
@@ -36,6 +38,12 @@ function findID(id) {
 	}
 }
 
+function pickQuestion() {
+	return questions[getRandomInt(questions.length)];
+}
+
+
+question = pickQuestion();
 app.use(express.json());
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: true }));
@@ -54,23 +62,63 @@ app.get('/test', (req, res, next) => {
   res.status(200).end("test");
 });
 
+app.post('/update', (req, res, next) => {
+	if (check_ids(req.body.id,req.body.password)) {
+		id = findID(req.body.id);
+		ans = {player_list: player_list,player: undefined};
+		if (players[id].updates) {ans.player=players[id];}
+		res.status(201).json(ans);
+		players[id].updates=[];
+	} else {res.status(401).json({message:"Erreur d'authentification"}); console.log('Erreur d\'authentification');}
+	
+	
+});
+
 app.post('/register_player', (req, res, next) => {
-  var id=players.length+1;
+  var id=players.at(-1).id+1;
   var crypto = require("crypto");
   var pswd = crypto.randomBytes(20).toString('hex');
-  player={name: req.body.name, password: pswd, id: id, cards: generateCards(), cards_played: []};
+  player={name: req.body.name, password: pswd, cards_played: [], id: id, cards: generateCards(), updates: ['new_player']};
+  for (let joueur of players) {
+	  joueur.updates.push("new_player");
+  }
   players.push(player);
+  player_list.push({name: player.name, id: id, nbr_votes: 0, score: 0, cards_played: []});
+  console.log(player.name+" a rejoint la partie. "+player_list.length+" joueurs.");
+  console.log(player_list);
+  
   res.status(201).json(player);
+});
+
+app.post('/player_left', (req, res, next) => {
+  if (check_ids(req.body.id,req.body.password)) {
+		id = findID(req.body.id);
+		player=players.splice(id,1)[0];
+		player_list.splice(id,1);
+		for (joueur of players) {
+			joueur.updates.push("player_left");
+		} 
+		console.log(player.name+" a quitté la partie. "+player_list.length+" joueurs.");
+		console.log(player_list);
+	} else {res.status(401).json({message:"Erreur d'authentification"}); console.log('Erreur d\'authentification');}
+	
+});
+
+app.post('/get_question', (req, res, next) => {
+  if (check_ids(req.body.id,req.body.password)) {
+		res.status(201).json({question:question});
+	} else {res.status(401).json({message:"Erreur d'authentification"}); console.log('Erreur d\'authentification');}
+	
 });
 
 app.post('/send_cards', (req, res, next) => {
 	if (check_ids(req.body.id,req.body.password)) {  
 	  id = findID(req.body.id);
-	  players[id].cards_played=req.body.cards_played;
+	  player_list[id].cards_played=req.body.cards_played;
 	  players[id].cards=generateCards(players[id].cards);
-	  console.log(players[id].cards_played);
+	  console.log(player_list[id].cards_played);
 	  res.status(201).json(players[id]);
-	} else {res.status(401).json({message:"Erreur d'authentification"}); console.log('erreu');}
+	} else {res.status(401).json({message:"Erreur d'authentification"}); console.log('Erreur d\'authentification');}
 });
 
 app.get('/cards', (req, res, next) => {
---
+++ frontend/script.js
@@ -0,0 +1,336 @@
+mode=undefined;
+dragObj=undefined;
+oldObj=undefined;
+balise=undefined;
+player = {name:"test"};
+const url = "http://localhost:3000/";
+player_list =[];
+
+if (window.matchMedia('(min-width: 1000px)')) {
+	size = ["1.2vw","1.3vw","2vw","2.1vw"];
+} else if (window.matchMedia('(max-width: 1000px)')) {
+	console.log("ouais");
+	size = ["1.7vh","1.8vh","2.5vh","2.6vh"];
+}
+playerName="herroP";
+for (var obj of document.querySelectorAll(".carte:not(.joue)")) {
+	obj.addEventListener('mousedown', initDnD);
+}
+
+document.addEventListener('mousemove', DnDMove);
+document.addEventListener("touchend", endDnD);
+
+
+function initQuestion(question) {
+	count=0;
+	while (question.search(/(?!>)□(?!<)/) > -1 ) {
+		question = question.replace(/(?!>)□(?!<)/,"<span class='balise' id='balise"+count.toString()+"'>□</span>");
+		newCarte=document.getElementById("carte_question").cloneNode(true);
+		newCarte.id="carte"+count.toString();
+		newCarte.style.visibility="visible";
+		newCarte.querySelector("h1").innerHTML="Carte "+(count+1).toString();
+		document.getElementById("bottom_plateau").appendChild(newCarte);
+		count+=1;
+	}
+	document.getElementById("question").children[1].innerHTML = question;
+}
+
+function getText(obj) {
+	if (obj.textContent) { // « textContent » existe ? Alors on s'en sert !
+			txt = obj.textContent;
+	} else if (obj.innerText) { // « innerText » existe ? Alors on doit être sous IE.
+		txt = obj.innerText + ' [via Internet Explorer]';
+	} else { // Si aucun des deux n'existe, cela est sûrement dû au fait qu'il n'y a pas de texte
+		txt = ''; // On met une chaîne de caractères vide
+	}
+	return txt;
+}
+
+function initDnD(e) {
+	if (mode!="VOTE") {
+		var x = e.currentTarget.offsetLeft;
+		var y = e.currentTarget.offsetTop;
+		dragObj=e.currentTarget.cloneNode(true);
+		e.currentTarget.style.visibility = "hidden"; 
+		oldObj=e.currentTarget;
+		dragObj.style.position="absolute";
+		dragObj.style.left=String(x)+"px";
+		dragObj.style.top=String(y)+"px";
+		document.body.addEventListener('mouseup', endDnD);
+		document.body.appendChild(dragObj);
+		
+	}
+}
+
+
+
+function DnDMove(e) {
+	if (dragObj != undefined) {
+		dragObj.style.left=String(e.clientX)+"px";
+		dragObj.style.top=String(e.clientY)+"px";
+	}
+	var x = e.clientX, y = e.clientY;
+	var width  = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+	var height = window.innerHeight|| document.documentElement.clientHeight|| document.body.clientHeight;
+	if (!(x>width || y>height || x<0 || y<0)) { //si on est toujours dans la fenêtre
+		var el=document.elementFromPoint(x,y);
+		baliseHover(el); //si on entre sur la balise, 
+	}
+	else { //cas où on sort de la fenêtre 
+		endDnD(e);
+	}
+	
+}
+
+
+function baliseHover(el, force = false) {
+	if (el.innerText=="□" || (balise && balise.innerText=="□")) { //On gère le hover lorsque les balises sont vides
+		if (balise && el.id != balise.id){ //cas où on sort de la balise 
+			if (balise.innerText != "□") { balise.style.fontSize = size[0]; } else {balise.style.fontSize = size[2];} //cas particulier où l'on vient d'une balise non vide
+			if (el.className=="balise") { //si on réentre dans l'autre balise. 
+				balise = el;
+				baliseHover(el);
+				return
+			 }
+			 else {
+				balise = undefined;
+			 }
+		}
+		else if (!balise && el.className=="balise") { //cas où on entre dans une balise 
+			balise = el;
+			balise.style.fontSize = size[3];	
+		}
+		
+		
+	}
+	else if (balise && el.className=="balise" && balise.id  != el.id){ //cas où on sort d'une balise pour entrer dans une autre
+		balise.style.color="white";
+		balise.style.fontSize = size[0];
+		balise = undefined;
+		baliseHover(el);
+		}
+	else if (el.className=="balise" && force == false) { //si l'élémnt parcouru est une balise et que force est faux
+		balise = el;
+		if (getComputedStyle(balise).color != "rgb(255, 255, 0)") { //si on est pas déjà entré sur l'objet
+			balise.style.color="rgb(255, 255, 0)";
+			balise.style.fontSize = size[1];
+		}
+	}
+	else if (balise && getComputedStyle(balise).color == "rgb(255, 255, 0)") { //deux cas : fin du DnD & la balise identifiée est jaune 																							//OU sortie de la balise
+		balise.style.color="white";
+		if (force == false) {balise.style.fontSize = size[0];} //cas où on termine le DnD
+		balise = undefined;
+	}
+}
+
+function insertCard(balise,card) {
+	if (balise.innerHTML!="□") {
+		addCard(balise.innerHTML);
+	}
+	else {
+		balise.style.backgroundColor="#6a9fab";
+	}
+	balise.innerHTML=card.querySelector("p").innerHTML.toLowerCase();
+	balise.style.fontSize=getComputedStyle(balise.parentNode).fontSize;
+	balise.addEventListener("mousedown",removeCard);
+	id="carte"+balise.id.slice(-1);
+	document.getElementById(id).className="carte";
+	document.getElementById(id).querySelector("p").innerHTML=card.querySelector("p").innerHTML;
+	player.cards_played[parseInt(id.slice(-1))]=card.querySelector("p").innerHTML;
+	
+}
+function addCard(text) {
+	new_card=document.getElementById("main").children[0].cloneNode(true);
+	text=text.charAt(0).toUpperCase() + text.slice(1);
+	new_card.querySelector("p").innerHTML=text;
+	new_card.querySelector("h1").innerHTML=playerName;
+	new_card.style.visibility="visible";
+	document.getElementById("main").appendChild(new_card);
+	new_card.addEventListener('mousedown', initDnD);
+}
+function removeCard(e) {
+	if (balise && balise.innerText!="□" && mode != "VOTE") { 
+		carte=document.getElementById("carte"+balise.id.slice(-1));
+		carte.querySelector("p").innerText="Placez une carte dans un trou.";
+		carte.className="carte joue vide";
+		addCard(balise.innerText);
+		balise.innerText="□";
+		balise.style.fontSize=size[2];
+		balise.style.backgroundColor="transparent";
+		player.cards_played[parseInt(balise.id.slice(-1))]=undefined;
+
+	}				
+}
+
+function deleteCards() {
+	let main =  document.getElementById("main");
+	while (main.children.length > 1) {
+		main.removeChild(main.lastChild);
+	}
+}
+
+function endDnD(e) {
+	
+	if (mode!="VOTE" && dragObj != undefined) {
+		oldObj.style.visibility = "visible";
+		if (balise) {
+			insertCard(balise,oldObj);
+			oldObj.parentNode.removeChild(oldObj);
+			document.body.removeChild(dragObj);
+			document.body.removeEventListener('mouseup', endDnD);
+			dragObj=undefined;
+			baliseHover(balise,true);
+			return;
+		}
+			
+		
+
+		document.body.removeChild(dragObj);
+		document.body.removeEventListener('mouseup', endDnD);
+		dragObj=undefined;
+	}
+	
+}
+
+function touchHandler(event) {
+	var touch = event.changedTouches[0];
+	var simulatedEvent = document.createEvent("MouseEvent");
+		simulatedEvent.initMouseEvent({
+		touchstart: "mousedown",
+		touchmove: "mousemove",
+		touchend: "mouseup"
+	}[event.type], true, true, window, 1,
+		touch.screenX, touch.screenY,
+		touch.clientX, touch.clientY, false,
+		false, false, false, 0, null);
+	touch.target.dispatchEvent(simulatedEvent);
+}
+
+function updateScores() {
+	content = document.querySelector("#score > #content");
+	for (joueur of player_list) {
+		if (content.querySelector("#player_"+joueur.id)) {
+			content.querySelector("#points_"+joueur.id).innerText=joueur.score;
+			content.querySelector("#nbr_votes_"+joueur.id).innerText=joueur.nbr_votes;
+		} else {
+			newDiv = document.createElement("div");
+			newDiv.id="player_"+joueur.id;
+			newDiv.innerHTML=`
+				<p><strong>`+joueur.name+`</strong> : <span id="points_`+joueur.id+`">`+joueur.score+`</span> points</p>
+				<p class="vote" id="vote_`+joueur.id+`"><span id="nbr_votes_`+joueur.id+`">`+joueur.nbr_votes+`</span> vote(s)</p>
+			`
+			content.appendChild(newDiv);
+		}
+	}
+}
+
+function getsa(type="",) {
+	$.get("http://localhost:3000/"+type, function (data, status){
+		console.log(typeof(data));
+		});
+}
+
+
+function register(pseudo) {
+
+	$.post(url+"register_player",{name:pseudo}, function (data, status){
+				if (status == "success") {
+					player = data;
+					for (card in player.cards) {
+						addCard(player.cards[card]);
+					}
+					window.parent.document.title = player.name;
+				}
+				
+		});
+			
+}
+
+function getQuestion() {
+	$.post(url+"get_question",{id:player.id,password:player.password}, function (data, status){
+		initQuestion(data.question);
+		});
+}
+
+function quit(e) {
+	e.preventDefault();
+	$.post(url+"player_left",{id:player.id,password:player.password}, function (data, status){
+				
+				
+		});
+}
+function deletePlayer() {
+	content = document.querySelector("#score > #content");
+	liste_id = player_list.map(function (el) {return el.id});
+	for (let i = 0; i<content.children.length; i++) {
+		if (!liste_id.includes(parseInt(content.children[i].id.split("_").at(-1)))) {
+			content.removeChild(content.children[i]);
+			i--;
+		}
+	}
+}
+function update() {
+	$.post(url+"update",{id:player.id,password:player.password}, function (data, status){
+		if (status == "success") {
+				player_list=data.player_list;
+				if (data.player) {
+					for (up of data.player.updates) {
+						if (up == "new_player") {
+							updateScores();
+						} else if (up == "player_left") {
+							console.log(player_list);
+							deletePlayer();
+						}
+					}
+				}
+					
+			}		
+		});
+}
+
+function sendCards() {
+	$.post(url+"send_cards",{id:player.id,password:player.password,cards_played:player.cards_played}, function (data, status){
+			if (status == "success") {
+				mode = "VOTE";
+				player = data;
+				deleteCards();
+				for (card in player.cards) {
+						addCard(player.cards[card]);
+					}
+					
+			}		
+	});
+}
+
+
+function stop() {
+	if (timer) {clearInterval(tick);
+		timer=null;
+		document.getElementById("btn").innerText="Allumer";
+		console.log("éteint");}
+	else {tick = setInterval(function(){update();}, 2000);timer=true;console.log("allumé");document.getElementById("btn").innerText="Eteindre";}
+}
+document.addEventListener("touchstart", touchHandler);
+document.addEventListener("touchmove", touchHandler);
+window.addEventListener('beforeunload', quit); 
+
+register(prompt("Entrez votre pseudo", ""));
+
+timer=true;
+
+
+
+tick = setInterval(function(){
+    
+	update();
+	
+}, 2000);
+
+setTimeout(function () {
+	getQuestion();
+},2000);
+
+
+
+
+
